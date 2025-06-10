c08ed6f4e0a61bdb84b6e16b10c8ea937415f3e6
jcgentry <jcgentry@mac.com>
2025-06-10 14:31:55 -0400

    Create stub helper to get all tags.

--- shared/src/jvmMain/kotlin/com/jacagen/jrecipe/llm/recipeChatBot.kt
+++ shared/src/jvmMain/kotlin/com/jacagen/jrecipe/llm/recipeChatBot.kt
@@ -2,6 +2,7 @@ package com.jacagen.jrecipe.llm
 
 import com.jacagen.jrecipe.service.EvernoteToLlmConverter
 import com.jacagen.jrecipe.service.RecipeChatBot
+import com.jacagen.jrecipe.service.RecipeHelper
 import dev.langchain4j.memory.chat.MessageWindowChatMemory
 import dev.langchain4j.model.openai.OpenAiChatModel
 import dev.langchain4j.service.AiServices
@@ -9,5 +10,6 @@ import dev.langchain4j.service.AiServices
 val recipeBot = AiServices.builder(RecipeChatBot::class.java)
     .chatModel(model)
     .chatMemory(MessageWindowChatMemory.withMaxMessages(100))
+    .tools(RecipeHelper())
     .build()
 
--- shared/src/jvmMain/kotlin/com/jacagen/jrecipe/service/RecipeChatBot.kt
+++ shared/src/jvmMain/kotlin/com/jacagen/jrecipe/service/RecipeChatBot.kt
@@ -1,8 +1,14 @@
 package com.jacagen.jrecipe.service
 
+import dev.langchain4j.agent.tool.Tool
 import dev.langchain4j.service.UserMessage
 
 interface RecipeChatBot {
     @UserMessage("{{it}}")
     fun chat(message: String): String     // Later add streamining?
+}
+
+class RecipeHelper {
+    @Tool
+    fun getAllTags() = setOf("Breakfast", "Drink")
 }
