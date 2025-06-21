70b4cfcbb8cbedd78d7da731d245949371ee0a76
jcgentry <jcgentry@mac.com>
2025-06-21 11:53:58 -0400

    Get Kotlin MUI working properly

--- web/src/jsMain/kotlin/com/jacagen/jrecipe/App.kt
+++ web/src/jsMain/kotlin/com/jacagen/jrecipe/App.kt
@@ -1,6 +1,7 @@
 package com.jacagen.jrecipe
 
 // web/src/jsMain/kotlin/App.kt
+import mui.material.Button
 import react.FC
 import react.Props
 import react.dom.html.ReactHTML.div
@@ -8,5 +9,10 @@ import react.dom.html.ReactHTML.div
 val App = FC<Props> {
     div {
         +"Hello from Kotlin/React!"
+    }
+    div {
+        Button {
+            +"Click Me"
+        }
     }
 }
